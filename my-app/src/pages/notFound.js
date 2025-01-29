167061f037a1850e8f74048d12f2750ea61917e7
DevMayani <106169881+DevMayani@users.noreply.github.com>
2025-01-29 17:19:04 +0100

    add

--- my-app/src/pages/notFound.js
+++ my-app/src/pages/notFound.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
+  const [bgImage, setBgImage] = useState('/assets/errorimg.svg');
+
+  useEffect(() => {
+    const updateBgImage = () => {
+      if (window.innerWidth >= 768) {
+        setBgImage('/assets/errorimg.svg'); // Desktop background
+      } else {
+        setBgImage('/assets/errorimg.svg'); // Mobile background
+      }
+    };
+
+    updateBgImage(); // Set the initial image
+    window.addEventListener('resize', updateBgImage); // Update on resize
+
+    return () => window.removeEventListener('resize', updateBgImage); // Cleanup
+  }, []);
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center">
+
+    <div className="flex flex-col items-center justify-center h-screen text-center" style={{
+      backgroundImage: `url('${bgImage}')`,
+      backgroundPosition: 'center top',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    }}>
       <h1 className="text-4xl font-bold text-red-500">404 - Page Not Found</h1>
       <p className="text-lg text-gray-600 mt-2">
         Oops! The page you are looking for does not exist.
